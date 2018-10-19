f31142a92a3da9d6badbe759512a2c01b78fe2f5
clam3154 <clam3154@bths.edu>
2018-10-18 22:06:22 -0400

    4

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
  */
 public class ChatBotRunner
 {
-
 	/**
 	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
 	 */
@@ -18,21 +17,66 @@ public class ChatBotRunner
 		ChatBotMir chatbot2 = new ChatBotMir();
 		ChatBotSheba chatbot3 = new ChatBotSheba();
 		ChatBotCalvin chatbot4 = new ChatBotCalvin();
-		
+
+		int chatbot = 1;
+		int cuisine = 1;
 
 		Scanner in = new Scanner (System.in);
 		System.out.println("Welcome to the chatbot place, nice to meet you!\nDo you want to talk to the traveler(1), the chef(2), the teacher(3), or the riddler(4)?");
 		String statement = in.nextLine();
-
+		if (statement.equals("1") || statement.equals("traveler") || statement.equals("the traveler"))
+		{
+			chatbot = 1;
+		}
+		if (statement.equals("2") || statement.equals("chef") || statement.equals("the chef"))
+		{
+			chatbot = 2;
+		}
+		if (statement.equals("3") || statement.equals("teacher") || statement.equals("the teacher"))
+		{
+			chatbot = 3;
+		}
+		if (statement.equals("4") || statement.equals("riddler") || statement.equals("the riddler"))
+		{
+			chatbot = 4;
+		}
+		System.out.println("Which cuisine do you want to talk about American(1), Mexican(2), or Indian(3).");
+		String statement2 = in.nextLine();
+		statement.toLowerCase();
+		if (statement2.equals("1") || statement2.equals("american"))
+		{
+			cuisine = 1;
+		}
+		if (statement2.equals("2") || statement2.equals("mexican"))
+		{
+			cuisine = 2;
+		}
+		if (statement2.equals("3") || statement2.equals("indian"))
+		{
+			cuisine = 3;
+		}
 
 		while (!statement.equals("Bye"))
 		{
 			//Use Logic to control which chatbot is handling the conversation\
 			//This example had only chatbot1
 
-
-
-			chatbot2.chatLoop(statement);
+			if(chatbot == 1)
+			{
+				chatbot1.chatLoop(statement);
+			}
+			if(chatbot == 2)
+			{
+				chatbot2.chatLoop(statement);
+			}
+			if(chatbot == 3)
+			{
+				chatbot3.chatLoop(statement, cuisine);
+			}
+			if(chatbot == 4)
+			{
+				chatbot4.chatLoop(statement, cuisine);
+			}
 
 
 			statement = in.nextLine();
